403e70c2643a7b5d269f8fafcdda357cf8b3d923
Lucas <lucas.s.augusto@hotmail.com>
2025-04-30 21:39:08 -0300

    Leaderboard basic layout finished

--- js/buttons.js
+++ js/buttons.js
@@ -2,29 +2,55 @@ const menu = document.getElementById("menu");
 const mainMenu = document.getElementById("main-menu");
 const optionsMenu = document.getElementById("options-menu");
 const board = document.getElementById("board");
-const startButton = document.getElementById("start-button");
-const optionsButton = document.getElementById("options-button");
-const menuButton = document.getElementById("menu-button");
-const returnButton = document.getElementById("return-button");
+const leaderboard = document.getElementById("leaderboard");
+const modeButtons = document.querySelectorAll(".mode-button");
 
 let mode = "normal"; // Default mode
 
-startButton.addEventListener("click", function() {
+document.getElementById("start-button").addEventListener("click", function() {
     menu.classList.add("hidden");
     board.classList.remove("hidden");
 });
 
-menuButton.addEventListener("click", function() {
+document.getElementById("menu-button").addEventListener("click", function() {
     board.classList.add("hidden");
     menu.classList.remove("hidden");
 });
 
-optionsButton.addEventListener("click", function() {
-    optionsMenu.classList.remove("hidden");
-    mainMenu.classList.add("hidden");
+document.getElementById("leaderboard-button").addEventListener("click", function() {
+    leaderboard.style.width = "var(--leaderboard-width)";
+    leaderboard.style.color = "var(--visible-color)";
 });
 
-returnButton.addEventListener("click", function() {
-    optionsMenu.classList.add("hidden");
-    mainMenu.classList.remove("hidden");
+document.getElementById("close-leaderboard-button").addEventListener("click", function() {
+    leaderboard.style.width = "0px";
+    leaderboard.style.color = "var(--font-hidden-color)";
+});
+
+function toggleMenu() {
+    if (mainMenu.classList.contains("hidden")) {
+        optionsMenu.classList.add("hidden");
+        mainMenu.classList.remove("hidden");
+    } else {
+        optionsMenu.classList.remove("hidden");
+        mainMenu.classList.add("hidden");
+    }
+}
+
+modeButtons.forEach(button => {
+    button.addEventListener("click", function() {
+        switch (button.id) {
+            case "normal-button":
+                mode = "normal";
+                break;
+            case "hard-button":
+                mode = "hard";
+                break;
+            case "easy-button":
+                mode = "easy";
+                break;
+        }
+
+        console.log("Selected mode:", mode);
+    });
 });
